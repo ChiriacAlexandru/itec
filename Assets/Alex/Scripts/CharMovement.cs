8d28e383582ce556009fea734339a342ed577e1c
ChiriacAlexandru <tagestudio.alexandru@gmail.com>
2025-04-04 19:52:04 +0300

    cinemachine

---
+++ Assets/Alex/Scripts/CharMovement.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class CharMovement : MonoBehaviour
+{
+    [Header("Can Move Bools")]
+    public bool canMoveLeft = false;
+    public bool canMoveRight = false;
+    public bool canMove2d= false;
+    public bool canJump = false;
+    public bool canMove3d= false;
+
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
